12e6fe8b833a9ffa946993244f45ec496035514e
Bertrand Ravier <bertrand.ravier-ext@aldautomotive.com>
2024-01-26 10:53:56 +0100

    feat: add CVA examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormBuilder,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CustomInputComponent } from './custom-input/custom-input.component';
+
+@Component({
+  selector: 'custom-comp',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  host: { '(blur)': '_onTouched()' },
+  template: `
+    Custom:
+    <button (click)="onClick()">Counter is {{ counter }}</button>
+  `,
+  providers: [
+    { provide: NG_VALUE_ACCESSOR, multi: true, useExisting: CustomComp },
+  ],
+  styles: `:host { background: pink; height: 200px; display: flex; align-items: center; justify-content: center; }`,
+})
+export class CustomComp implements ControlValueAccessor {
+  counter = 2;
+
+  writeValue(value: number) {
+    this.counter = value;
+  }
+
+  onClick(): void {
+    this.onChange(++this.counter);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  onChange: (value: any) => void = () => {};
+  onTouched: () => void = () => {};
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [ReactiveFormsModule, JsonPipe, CustomComp, CustomInputComponent],
+  template: `<form [formGroup]="form">
+      <app-custom-input formControlName="first" />
+      <custom-comp formControlName="last" />
+    </form>
+    {{ form.value | json }}
+    <button (click)="form.reset()">RESET</button>`,
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'controlValueAccessor';
+  private readonly fb = inject(FormBuilder).nonNullable;
+
+  form = this.fb.group({
+    first: this.fb.control('test'),
+    last: this.fb.control(5),
+  });
 }
---
+++ src/app/custom-input/custom-input.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnDestroy } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-custom-input',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: CustomInputComponent,
+    },
+  ],
+  templateUrl: './custom-input.component.html',
+  styleUrl: './custom-input.component.scss',
+})
+export class CustomInputComponent implements OnDestroy, ControlValueAccessor {
+  input = new FormControl('');
+  subscriptions: Subscription[] = [];
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+
+  writeValue(input: string) {
+    this.input.setValue(input);
+  }
+
+  // Step 5: Listen to value changes on the form control. If something happens, the function provided by Angular will be called and the outside world will be informed.
+  registerOnChange(fn: any): void {
+    this.subscriptions.push(this.input.valueChanges.subscribe(fn));
+  }
+
+  onTouch: any = () => {};
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+}
